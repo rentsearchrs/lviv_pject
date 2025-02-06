288179b6663a89582d499fad05f23c9233801c43
rentsearchrs <rentsearchrs@gmail.com>
2025-02-06 07:07:19 +0100

    Update models.py

--- parser/models.py
+++ parser/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String, Text
-from database import Base
+from parser.database import Base
 from sqlalchemy.orm import relationship
 class Apartment(Base):
     __tablename__ = "apartments"
