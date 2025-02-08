04c2cf5ca9df8717109d6970c976f5d71cdd1135
rentsearchrs <rentsearchrs@gmail.com>
2025-02-08 12:48:47 +0100

    ok

--- parser/main.py
+++ parser/main.py
@@ -1315,4 +1315,4 @@ async def stop_scraping():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="https://lviv-pject.vercel.app", port=3000)
